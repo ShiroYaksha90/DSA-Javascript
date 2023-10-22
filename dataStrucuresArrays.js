143577c0964cfb2645964517c7644b4965c9546a
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-22 23:22:35 +0300

    Add sortMergedArray function

--- dataStrucuresArrays.js
+++ dataStrucuresArrays.js
@@ -111,5 +111,34 @@ console.log('reversed string: ', reversedString2('Hi my name is Basim'))
 // [0,3,4,4,6,30,31]
 
 const mergSortedArrays = (arr1,arr2) => {
+    const mergedArray=[]
+    let arr1Item= arr1[0]
+    let arr2Item = arr2[0]
+    let i =1
+    let j =1
+
+    if (arr1.length === 0){
+        return arr2
+    }
+
+    if(arr2.length === 0){
+        return arr1
+    }
+    while(arr1Item || arr2Item){
+        console.log(arr1Item,arr2Item)
+        // To optimize the solution we can refactore the if else statement as functions that returns the desired output
+        if (!arr2Item || arr1Item < arr2Item){
+            mergedArray.push(arr1Item)
+            arr1Item = arr1[i]
+            i++
+        }else {
+            mergedArray.push(arr2Item)
+            arr2Item = arr2[j]
+            j++
+        }
+    }
     
+    return mergedArray
 }
+
+console.log(mergSortedArrays([0,3,4,31], [4,6,30]))
