f34c1f3bf346f6a3064b6be0f9674d8fbfeb9705
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-25 21:47:02 +0300

    Create a hash table using a class

--- dataStructuresHashTables.js
+++ dataStructuresHashTables.js
@@ -10,4 +10,48 @@ let user = {
 }
 user.scream();
 user.spell = 'abra kadabra';
-console.log(user.spell)
+console.log(user)
+
+// Crate a hash table using a class
+
+class HashTable {
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+    // Hash function
+
+    _hash(key){
+        let hash = 0;
+        for(let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) %
+            this.data.length
+        }
+        return hash;
+    } 
+    set(key, value){
+        let address = this._hash(key);
+        if(!this.data[address]){
+            this.data[address] = [];
+        }
+        this.data[address].push([key,value]);
+        return this.data;
+    }
+
+    get(key){
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for(let i = 0; i < currentBucket.length; i++){
+                if(currentBucket[i][0] === key){
+                    return currentBucket[i][1]
+                }
+            }
+        }
+        return undefined;
+    }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+console.log(myHashTable.get('grapes'))
