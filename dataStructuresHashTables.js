3380abaceb410cfd5325a7a018c8a713080311a0
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-25 21:30:19 +0300

    Implement a hash table

---
+++ dataStructuresHashTables.js
@@ -0,0 +1,13 @@
+// Objective: Understand how hash tables work and how to implement them in Javascript
+
+let user = {
+    age :54,
+    name: 'Kylie',
+    magic: true,
+    scream: ()=> {
+        console.log('ahhhhhhh!');
+    }
+}
+user.scream();
+user.spell = 'abra kadabra';
+console.log(user.spell)
